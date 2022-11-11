37e9446a55c692b554d5b6529fb127e696c2c1d0
pierstab <pierstabilini@gmail.com>
2022-11-11 17:09:09 +0000

    minor changes, add airdrop module account

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 require("dotenv").config();
 
-const denom = process.env.DENOM || "ujuno";
+const denom = process.env.DENOM || "arebus";
 const interval = process.env.INTERVAL || 7200000;
+const total_max_supply = 1000000000
+const reb18 = 1000000000000000000
+// const total_block_reward_supply= 635708034
+const claim_wallet = process.env.AIRDROP_CLAIM_ACCOUNT;
+const block_reward_staking_percent = 0.72
 
 const vestingAccounts = process.env.VESTING_ACCOUNTS
   ? process.env.VESTING_ACCOUNTS.split(",")
@@ -82,21 +87,35 @@ async function updateData() {
       url: `${process.env.REST_API_ENDPOINT}/cosmos/mint/v1beta1/inflation`,
     });
 
+    // Get inflation
+    claim_module = await axios({
+      method: "get",
+      url: `${process.env.REST_API_ENDPOINT}/cosmos/bank/v1beta1/balances/${claim_wallet}`,
+    });
+    claimAirdropBalance = claim_module.data.balances[0].amount;
+    
+    current_token_emission = await axios({
+      method: "get",
+      url: `${process.env.REST_API_ENDPOINT}/cosmos/mint/v1beta1/annual_provisions`,
+    });
+    current_emission = current_token_emission.data.annual_provisions;
+
+    // normalize inflation (inflation is vs block_reward_pool not current total supply)
+    
     totalStaked = stakingInfo.data.pool.bonded_tokens;
     bondedRatio = totalStaked / totalSupply.data.amount.amount;
-    apr = inflation.data.inflation / bondedRatio;
+    apr = inflation.data.inflation / bondedRatio * block_reward_staking_percent;
 
     console.log("APR: ", apr);
     console.log("Total Staked: ", totalStaked);
     console.log("Bonded ratio: ", bondedRatio);
+    console.log("Airdrop module balance: ", claimAirdropBalance);
 
     // Loop through pool balances to find denom
     for (let i in communityPool.data.pool) {
       if (communityPool.data.pool[i].denom === denom) {
         console.log("Community pool: ", communityPool.data.pool[i].amount);
-
         communityPoolMainDenomTotal = communityPool.data.pool[i].amount;
-
         // Subtract community pool from total supply
         tmpCirculatingSupply =
           totalSupply.data.amount.amount - communityPool.data.pool[i].amount;
@@ -107,7 +126,7 @@ async function updateData() {
     bar1.start(vestingAccounts.length, 0);
     for (let i = 0; i < vestingAccounts.length; i++) {
       const account = await client.auth.account(vestingAccounts[i]);
-      let accountInfo = PeriodicVestingAccount.decode(account.value);
+      let accountInfo = ContinuousVestingAccount.decode(account.value);
       let originalVesting =
         accountInfo.baseVestingAccount.originalVesting[0].amount;
       let delegatedFree =
@@ -116,11 +135,15 @@ async function updateData() {
           : 0;
       tmpCirculatingSupply -= originalVesting - delegatedFree;
       bar1.update(i);
-    }
+    }    
     bar1.update(vestingAccounts.length);
     bar1.stop();
+    
+    // remove from circulating supply the airdrop module account balance.
+    tmpCirculatingSupply -= claimAirdropBalance;
     circulatingSupply = tmpCirculatingSupply;
-    console.log("Circulating supply: ", circulatingSupply);
+
+    console.log("Circulating supply: ", circulatingSupply/reb18);
     fs.writeFile('circulating_supply.txt', circulatingSupply.toString(), function (err) {
       if (err) return console.log(err);
     });
@@ -139,50 +162,23 @@ setInterval(updateData, interval);
 app.get("/", async (req, res) => {
   res.json({
     apr,
-    bondedRatio,
-    circulatingSupply: Decimal.fromAtomics(circulatingSupply.toString(), 6).toString(),
-    communityPool: Decimal.fromAtomics(
-      communityPoolMainDenomTotal.split(".")[0],
-      6
-    ).toString(),
-    denom: denom.substring(1).toUpperCase(),
-    totalStaked: Decimal.fromAtomics(totalStaked, 6).toString(),
-    totalSupply: Decimal.fromAtomics(
-      totalSupply.data.amount.amount,
-      6
-    ).toString(),
+    bonded_ratio: bondedRatio,    
+    circulating_supply: (circulatingSupply/reb18).toString(),   
+    community_pool: (communityPoolMainDenomTotal/reb18).toString(),  
+    denom: denom.substring(1).toUpperCase(),   
+    total_staked: (totalStaked/reb18).toString(),
+    total_supply: (totalSupply.data.amount.amount/reb18).toString()
   });
 });
 
-app.get("/apr", async (req, res) => {
-  res.send(apr.toString());
-});
-
-app.get("/bonded-ratio", async (req, res) => {
-  res.send(bondedRatio.toString());
-});
-
 app.get("/circulating-supply", async (req, res) => {
-  res.send(Decimal.fromAtomics(circulatingSupply.toString(), 6).toString());
-});
-
-app.get("/total-staked", async (req, res) => {
-  res.send(Decimal.fromAtomics(totalStaked, 6).toString());
+  res.send((circulatingSupply/reb18).toString());
 });
 
 app.get("/total-supply", async (req, res) => {
-  res.send(Decimal.fromAtomics(totalSupply.data.amount.amount, 6).toString());
+  res.send((totalSupply.data.amount.amount/reb18).toString());
 });
 
-app.get("/community-pool", async (req, res) => {
-  res.send(
-    Decimal.fromAtomics(communityPoolMainDenomTotal.split(".")[0], 6).toString()
-  );
-});
-
-app.get("/denom", async (req, res) => {
-  res.send(denom.substring(1).toUpperCase());
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
